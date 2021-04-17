f2edfb013e39d4a1488bb1198683c3ca5daa0897
ym <2371579340@qq.com>
2021-04-17 20:12:21 +0800

    登录和权限

--- yeb1/yeb-server/src/main/java/com/xxxx/server/service/IMenuService.java
+++ yeb1/yeb-server/src/main/java/com/xxxx/server/service/IMenuService.java
@@ -2,6 +2,9 @@ package com.xxxx.server.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.xxxx.server.pojo.Menu;
+import com.xxxx.server.pojo.Role;
+
+import java.util.List;
 
 /**
  * <p>
@@ -13,4 +16,9 @@ import com.xxxx.server.pojo.Menu;
  */
 public interface IMenuService extends IService<Menu> {
 
+    //根据登录用户名查询拥有的权限
+    List<Menu> getMenuByAdminName();
+
+    //根据当前资源，查询可使用他的角色
+    List<Menu> getRoleByMenuId();
 }
