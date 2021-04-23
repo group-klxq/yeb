7da163f36b7f767002d3f2dbd83256a2d9c8d535
lio <1139674521@qq.com>
2021-04-23 10:38:17 +0800

    李旭东最新的上传

--- yeb1/yeb-server/src/main/java/com/xxxx/server/config/MybatisPlusConfig.java
+++ yeb1/yeb-server/src/main/java/com/xxxx/server/config/MybatisPlusConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-class MyBatisPlusConfig extends Page {
+public class MyBatisPlusConfig extends Page {
 
     //自动分页
     @Bean
